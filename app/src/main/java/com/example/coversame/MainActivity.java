280cfcfb9c06175bc7b7de43fa63dd2ede888423
Vadim Lipakov <revalier01@mail.ru>
2019-09-07 12:34:18 +0300

    - connected lib for deezer-api.

---
+++ app/src/main/java/com/example/coversame/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.coversame;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.loadApi)
+    Button loadApi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+}
